13090a1711328cb1b65cecab011f611ccc89fd96
wenbo <yiyun6674@hotmail.com>
2016-09-18 16:12:27 +0800

    inited and added doc/cmd.sh

---
+++ doc/cmd.sh
@@ -0,0 +1,8 @@
+sed -i "s/原字符串/新字符串/g" `grep 原字符串 -rl 所在目录`
+LC_ALL=C sed -i "" "s/yafeilee.me/zhuwenqian.com/g" `grep yafeilee.me -rl ./`
+LC_ALL=C sed -i "" "s/AliceBlog/AliceBlog/g" `grep AliceBlog -rl ./`
+LC_ALL=C sed -i "" "s/李亚飞/Victorialice/g" `grep 李亚飞 -rl ./`
+LC_ALL=C sed -i "" "s/WinDy/Alice/g" `grep WinDy -rl ./`
+LC_ALL=C sed -i "" "s/windy/victorialice/g" `grep windy -rl ./`
+LC_ALL=C sed -i "" "s/victorialice@gmail.com/1506675573@qq.com/g" `grep victorialice@gmail.com -rl ./`
+LC_ALL=C sed -i "" "s/victorialice/victorialice/g" `grep victorialice -rl ./`
