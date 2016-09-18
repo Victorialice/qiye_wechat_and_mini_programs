f7f2121df4c43c88c42ca7655d101279e6f48357
wenbo <yiyun6674@hotmail.com>
2016-09-18 16:46:34 +0800

    slimrb --rails -e index.html.slim > index.html.erb

--- doc/cmd.sh
+++ doc/cmd.sh
@@ -6,3 +6,5 @@ LC_ALL=C sed -i "" "s/WinDy/Alice/g" `grep WinDy -rl ./`
 LC_ALL=C sed -i "" "s/windy/victorialice/g" `grep windy -rl ./`
 LC_ALL=C sed -i "" "s/lyfi2003@gmail.com/1506675573@qq.com/g" `grep lyfi2003@gmail.com -rl ./`
 LC_ALL=C sed -i "" "s/lyfi2003/victorialice/g" `grep lyfi2003 -rl ./`
+
+slimrb --rails -e index.html.slim > index.html.erb
